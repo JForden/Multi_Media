2fec76a7ada3b5f52ea9f728ca6cb204a0b40393
Riordan Brennan <riordan_brennan@yahoo.com>
2021-12-03 12:55:08 -0600

    Profile page updates

--- multimedia/lib/profile.dart
+++ multimedia/lib/profile.dart
@@ -8,22 +8,70 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+    Container _buildBigButton(Color color, IconData icon, String text) {
+      return Container(
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+              color: Colors.amber, borderRadius: BorderRadius.circular(16)),
+          padding: EdgeInsets.all(0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, color: color, size: 64),
+              Container(
+                constraints: BoxConstraints(minWidth: 110, maxWidth: 110),
+                margin: const EdgeInsets.only(top: 8),
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: color,
+                  ),
+                  softWrap: true,
+                ),
+              )
+            ],
+          ));
+    }
+
+    Padding _buildBigRows(Container button1, Container button2) {
+      return Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              button1,
+              button2,
+            ],
+          ));
+    }
+
     Color color = Color.fromARGB(255, 71, 57, 45);
 
     Widget topBar = const Padding(
       padding: EdgeInsets.all(32),
       child: Text(
-        'Settings',
+        'Profile    Settings!',
         softWrap: true,
       ),
     );
-    Widget bigButtons = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Buttons!',
-        softWrap: true,
-      ),
-    );
+
+    Widget bigButtons = Padding(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildBigRows(_buildBigButton(color, Icons.star, 'Donate'),
+                _buildBigButton(color, Icons.star, 'Become a Member')),
+            _buildBigRows(_buildBigButton(color, Icons.shopping_bag, 'Shop'),
+                _buildBigButton(color, Icons.help, 'Help')),
+          ],
+        ));
 
     return Scaffold(
       body: ListView(
