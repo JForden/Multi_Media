f82f37573b2cb5b3375d9009d043d776271d7df9
Jack Forden <60225273+JForden@users.noreply.github.com>
2021-12-03 14:40:27 -0600

    removed commented code added small replace change

--- multimedia/lib/articles.dart
+++ multimedia/lib/articles.dart
@@ -24,7 +24,8 @@ class _ArticlePageState extends State<ArticlePage> {
                   itemBuilder: (context, index) {
                     String title1 = snapshot.data[index]['title']['rendered']
                         .replaceAll('&#8216;', '\'')
-                        .replaceAll('&#8217;', '\'');
+                        .replaceAll('&#8217;', '\'')
+                        .replaceAll('&#038;', '&');
                     return ListTile(
                       title: Text(
                         title1,
@@ -50,43 +51,3 @@ class _ArticlePageState extends State<ArticlePage> {
     );
   }
 }
-
-// class _ArticlePageState extends State<ArticlePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: <Widget>[
-//             Container(
-//               decoration: BoxDecoration(
-//                 image: DecorationImage(
-//                   image: AssetImage("assets/images/bg.jpg"),
-//                   fit: BoxFit.cover,
-//                 ),
-//               ),
-//               child: Image.network(
-//                 'https://s3.amazonaws.com/libapps/accounts/9189/images/newspaper.jpg',
-//                 width: 200.0,
-//               ),
-//             ),
-//             Text('Example Article Link 1',
-//                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//             Image.network(
-//                 'https://s3.amazonaws.com/libapps/accounts/9189/images/newspaper.jpg'),
-//             Text('Example Article Link 2',
-//                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//             Image.network(
-//                 'https://s3.amazonaws.com/libapps/accounts/9189/images/newspaper.jpg'),
-//             Text('Example Article Link 3',
-//                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//             Image.network(
-//                 'https://s3.amazonaws.com/libapps/accounts/9189/images/newspaper.jpg'),
-//             Text('Example Article Link 4',
-//                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
