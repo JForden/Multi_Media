bbb9d234b92c087f2e0748168eed752baaf1c493
Tyler Wenslaff <tyler.wenslaff@marquette.edu>
2021-12-04 22:36:09 -0600

    Updated Playlist Page

--- multimedia/lib/playlists.dart
+++ multimedia/lib/playlists.dart
@@ -1,6 +1,3 @@
-import 'dart:html';
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -12,11 +9,27 @@ class PlaylistPage extends StatefulWidget {
   _PlaylistPageState createState() => _PlaylistPageState();
 }
 
+class SongInfo {
+  String songName;
+  String artistName;
+  String albumName;
+  String dj;
+
+  SongInfo(
+      {required this.songName,
+      required this.artistName,
+      required this.albumName,
+      required this.dj});
+}
+
 class _PlaylistPageState extends State<PlaylistPage> {
   final ScrollController _scrollController = ScrollController();
   List<String> items = [];
+  List<SongInfo> songInfoList = [];
   bool loading = false;
   bool allLoaded = false;
+  bool isHover = false;
+  String previousDJ = "None";
 
   mockFetch() async {
     if (allLoaded) {
@@ -26,15 +39,33 @@ class _PlaylistPageState extends State<PlaylistPage> {
       loading = true;
     });
     await Future.delayed(const Duration(milliseconds: 500));
-    List<String> newData = items.length >= 60
+    List<SongInfo> initialData = songInfoList.length >= 60
         ? []
-        : List.generate(20, (index) => "Example Song #${index + items.length}");
-    if (newData.isNotEmpty) {
-      items.addAll(newData);
+        : List.generate(
+            20,
+            (index) => SongInfo(
+                songName: "Interesting Song #${index + songInfoList.length}",
+                artistName: "Example Artist",
+                albumName: "Example Album Name",
+                dj: "Example DJ"));
+    if (initialData.isNotEmpty) {
+      initialData[10].dj = "That Other DJ";
+      initialData[10].songName = "That Other Interesting Song";
+      for (int i = 0; i < initialData.length; i++) {
+        if (previousDJ != "None" && previousDJ != initialData[i].dj) {
+          songInfoList.add(SongInfo(
+              songName: "None",
+              artistName: "None",
+              albumName: "None",
+              dj: initialData[i].dj));
+        }
+        songInfoList.add(initialData[i]);
+        previousDJ = initialData[i].dj;
+      }
     }
     setState(() {
       loading = false;
-      allLoaded = newData.isEmpty;
+      allLoaded = initialData.isEmpty;
     });
   }
 
@@ -71,27 +102,61 @@ class _PlaylistPageState extends State<PlaylistPage> {
       // appBar: AppBar(
       //     centerTitle: true,
       //     title: const Text("Example DJ" " ON AIR"),
-      //     backgroundColor: Colors.brown),
+      //     backgroundColor: const Color.fromARGB(212, 92, 71, 49)),
+
       body: LayoutBuilder(builder: (context, constraints) {
-        if (items.isNotEmpty) {
+        if (songInfoList.isNotEmpty) {
           return Column(children: [
             Container(
               height: 50,
-              color: Colors.brown,
+              color: const Color.fromARGB(212, 92, 71, 49),
               width: constraints.maxWidth,
               child: Row(children: [
-                Text(formattedDate),
-                const Text(
-                  'DJ ON AIR',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'ON AIR:',
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Row(children: [
+                        const CircleAvatar(
+                          radius: 9.0,
+                          backgroundImage:
+                              AssetImage('assets/images/loading.gif'),
+                          backgroundColor: Colors.transparent,
+                        ),
+                        Text(
+                          ' ' + songInfoList[0].dj,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ]),
+                    ]),
                 InkWell(
-                  child: const Text('Search'),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Search',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
                   onTap: () =>
                       launch('https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
                 ),
@@ -106,38 +171,105 @@ class _PlaylistPageState extends State<PlaylistPage> {
                     return Container(
                         decoration: BoxDecoration(
                             border: Border.all(color: Colors.grey)),
-                        child: ListTile(
-                            //where each row is made
-                            leading: Image.asset(
-                              'assets/images/loading.gif',
-                            ),
-                            title: Text(items[index]),
-                            subtitle: const Text(
-                                "Example Artist" " - " "Example Album"),
-                            trailing: Column(
-                              children: [
-                                InkWell(
-                                    child: const Text("Spotify",
-                                        style: TextStyle(
-                                            height: 1.1, fontSize: 14)),
-                                    onTap: () => launch(
-                                        'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd')),
-                                InkWell(
-                                    child: const Text("Itunes",
-                                        style: TextStyle(
-                                            height: 1.1, fontSize: 14)),
-                                    onTap: () => launch(
-                                        'https://music.apple.com/us/album/never-gonna-give-you-up/1558533900?i=1558534271')),
-                                InkWell(
-                                    child: const Text("Amazon",
-                                        style: TextStyle(
-                                            height: 1.1, fontSize: 14)),
-                                    onTap: () => launch(
-                                        'https://www.amazon.com/Never-Gonna-Give-You-Up/dp/B07X66DCLM/ref=sr_1_1?crid=32G0JAXOGTXGR&keywords=never+gonna+give+you+up+rick+astley&qid=1638585041&s=dmusic&sprefix=never+gonn%2Cdigital-music%2C189&sr=1-1')),
-                              ],
-                            )));
+                        child: songInfoList[index].songName == "None"
+                            ? Container(
+                                height: 50,
+                                color: const Color.fromARGB(212, 92, 71, 49),
+                                width: constraints.maxWidth,
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const CircleAvatar(
+                                        radius: 9.0,
+                                        backgroundImage: AssetImage(
+                                            'assets/images/loading.gif'),
+                                        backgroundColor: Colors.transparent,
+                                      ),
+                                      Text(
+                                        ' ' + songInfoList[index].dj,
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ]),
+                              )
+                            : ListTile(
+                                //where each row is made
+                                leading: Image.asset(
+                                  'assets/images/loading.gif',
+                                ),
+                                title: Text(songInfoList[index].songName),
+                                subtitle: Text(songInfoList[index].artistName +
+                                    " - " +
+                                    songInfoList[index].albumName),
+                                trailing: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      InkWell(
+                                          hoverColor: Colors.white,
+                                          child: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: const [
+                                                CircleAvatar(
+                                                  radius: 7.0,
+                                                  backgroundImage: AssetImage(
+                                                      'images/spotify.png'),
+                                                  backgroundColor:
+                                                      Colors.transparent,
+                                                ),
+                                                Text(" Spotify",
+                                                    style: TextStyle(
+                                                      height: 1.1,
+                                                      fontSize: 14,
+                                                    ))
+                                              ]),
+                                          onTap: () => launch(
+                                              'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd')),
+                                      InkWell(
+                                          hoverColor: Colors.white,
+                                          child: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Image.asset(
+                                                  'images/apple.png',
+                                                  width: 14,
+                                                  height: 14,
+                                                ),
+                                                const Text(" Itunes",
+                                                    style: TextStyle(
+                                                        height: 1.1,
+                                                        fontSize: 14)),
+                                              ]),
+                                          onTap: () => launch(
+                                              'https://music.apple.com/us/album/never-gonna-give-you-up/1558533900?i=1558534271')),
+                                      InkWell(
+                                          hoverColor: Colors.white,
+                                          child: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: const [
+                                                CircleAvatar(
+                                                  radius: 8.0,
+                                                  backgroundImage: AssetImage(
+                                                      'images/amazon.jpg'),
+                                                  backgroundColor:
+                                                      Colors.transparent,
+                                                ),
+                                                Text(" Amazon",
+                                                    style: TextStyle(
+                                                        height: 1.1,
+                                                        fontSize: 14)),
+                                              ]),
+                                          onTap: () => launch(
+                                              'https://www.amazon.com/Never-Gonna-Give-You-Up/dp/B07X66DCLM/ref=sr_1_1?crid=32G0JAXOGTXGR&keywords=never+gonna+give+you+up+rick+astley&qid=1638585041&s=dmusic&sprefix=never+gonn%2Cdigital-music%2C189&sr=1-1')),
+                                    ]),
+                              ));
                   },
-                  itemCount: items.length),
+                  itemCount: songInfoList.length),
               if (loading) ...[
                 Positioned(
                     left: 0,
