98b60a5057a3edd815a108351a87275f1fc24b8e
Tyler Wenslaff <tyler.wenslaff@marquette.edu>
2022-02-28 20:47:17 -0600

    Added features and backend improvements to radio page

--- multimedia/lib/radio.dart
+++ multimedia/lib/radio.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'package:url_launcher/url_launcher.dart';
 
 
 class RadioPage extends StatefulWidget {
   @override
   _RadioPageState createState() => _RadioPageState();
-  // _ArticlePageState2 createState2() => _ArticlePageState2();
+}
+
+class CurrentSongInfo {
+  String currentSongName = "";
+  String currentArtistName = "";
+  String currentSongAlbum = "";
+  String currentDJ = "";
+
+  CurrentSongInfo(String currentSongName, String currentArtistName, String currentSongAlbum, String currentDJ)
+    {
+      this.currentSongName = currentSongName;
+      this.currentArtistName = currentArtistName;
+      this.currentSongAlbum = currentSongAlbum;
+      this.currentDJ = currentDJ;
+    }
 }
 
 class _RadioPageState extends State<RadioPage> {
+
+var songInfo = CurrentSongInfo(
+  "Bohemian Rhapsody",
+  "Queen",
+  "A Night At The Opera",
+  "Me"
+);
+
   @override
   Widget build(BuildContext context) {
     Widget titleSection = Container(
@@ -22,16 +44,16 @@ class _RadioPageState extends State<RadioPage> {
               children: [
                 /*2*/
                 Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Title of stream',
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    songInfo.currentSongName,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 Text(
-                  'Artist name',
+                  songInfo.currentArtistName + " - " + songInfo.currentSongAlbum,
                   style: TextStyle(
                     color: Colors.grey[500],
                   ),
@@ -48,9 +70,9 @@ class _RadioPageState extends State<RadioPage> {
     Widget buttonSection = Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        _buildButtonColumn(color, Icons.person_sharp, 'Artist info'),
-        _buildButtonColumn(color, Icons.list, 'Add to playlist'),
-        _buildButtonColumn(color, Icons.share, 'Share'),
+        _buildButtonColumn(color, Icons.person_sharp, 'Artist info', 'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd'),
+        _buildButtonColumn(color, Icons.list, 'Add to playlist', 'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd'),
+        _buildButtonColumn(color, Icons.share, 'Share', 'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd'),
       ],
     );
 
@@ -82,24 +104,27 @@ class _RadioPageState extends State<RadioPage> {
     );
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
+  InkWell _buildButtonColumn(Color color, IconData icon, String label, String link) {
+    return InkWell(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
+      onTap: () => launch(link)
     );
   }
 }
