b26bdcb9965376cc573fc32818eb92d9f205de2c
Tyler Wenslaff <tyler.wenslaff@marquette.edu>
2022-04-02 23:36:41 -0500

    Merge and Song Links

--- multimedia/lib/radio.dart
+++ multimedia/lib/radio.dart
@@ -1,70 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:multimedia/songs.dart';
 import 'package:url_launcher/url_launcher.dart';
-//import 'package:flutter_radio_player/flutter_radio_player.dart';
+import 'package:flutter_radio_player/flutter_radio_player.dart';
 
 class RadioPage extends StatefulWidget {
   @override
   _RadioPageState createState() => _RadioPageState();
 }
 
-class CurrentSongInfo {
-  String currentSongName = "";
-  String currentArtistName = "";
-  String currentSongAlbum = "";
-  String currentDJ = "";
-
-  CurrentSongInfo(String currentSongName, String currentArtistName,
-      String currentSongAlbum, String currentDJ) {
-    this.currentSongName = currentSongName;
-    this.currentArtistName = currentArtistName;
-    this.currentSongAlbum = currentSongAlbum;
-    this.currentDJ = currentDJ;
-  }
-}
-
 class _RadioPageState extends State<RadioPage> {
   IconData playIcon = Icons.play_arrow;
   bool playing = false;
-  var songInfo = CurrentSongInfo(
-      "Bohemian Rhapsody", "Queen", "A Night At The Opera", "Me");
 
   @override
   Widget build(BuildContext context) {
     double deviceWidth = MediaQuery.of(context).size.width;
     double deviceHeight = MediaQuery.of(context).size.height;
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    songInfo.currentSongName,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+    Widget titleSection(SongInfo currentSong) {
+      return Container(
+          padding: const EdgeInsets.all(32),
+          child: Row(
+            children: [
+              Expanded(
+                /*1*/
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /*2*/
+                    Container(
+                      //padding: EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        currentSong.songName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
+                    Text(
+                      currentSong.artistName + " - " + currentSong.albumName,
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  songInfo.currentArtistName +
-                      " - " +
-                      songInfo.currentSongAlbum,
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+              ),
+            ],
+          ));
+    }
 
     Color color = Theme.of(context).primaryColor;
 
@@ -75,43 +58,98 @@ class _RadioPageState extends State<RadioPage> {
       ],
     );
 
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.person_sharp, 'Artist info',
-            'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd'),
-        _buildButtonColumn(color, Icons.list, 'Add to playlist',
-            'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd'),
-        _buildButtonColumn(color, Icons.share, 'Share',
-            'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd'),
-      ],
-    );
+    Widget buttonSection(SongInfo currentSong) {
+      String artistUrl = "";
+      if (currentSong.artistSpotifyID != "") {
+        artistUrl =
+            'https://open.spotify.com/artist/' + currentSong.artistSpotifyID;
+      }
 
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Info about current stream',
-        softWrap: true,
-      ),
-    );
+      String? songID = currentSong.songSpotifyID;
+      String songUrl = "";
+      if (songID != null) {
+        songUrl = "https://open.spotify.com/track/$songID";
+      }
+
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(
+            color,
+            Icons.person_sharp,
+            'Artist info',
+            artistUrl,
+          ),
+          _buildButtonColumn(color, Icons.list, 'Add to playlist', songUrl),
+          _buildButtonColumn(color, Icons.share, 'Share',
+              'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=6eb186f21dd749bd'),
+        ],
+      );
+    }
+
+    Widget textSection(SongInfo currentSong) {
+      return Padding(
+        padding: EdgeInsets.all(32),
+        child: Text(
+          'Info about current stream',
+          softWrap: true,
+        ),
+      );
+    }
 
     return MaterialApp(
         title: 'Radio Page',
         home: Scaffold(
-          body: ListView(
-            children: [
-              Image.asset(
-                'assets/images/loading.gif',
-                width: 200,
-                height: 200,
-              ),
-              titleSection,
-              playButtonSection,
-              buttonSection,
-              textSection,
-            ],
-          ),
-        ),
+            body: Center(
+                child: FutureBuilder<Songs>(
+                    future: fetchSongs(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<dynamic> songInfoListFuture = snapshot.data!.songs;
+                        SongInfo currentSong = SongInfo(
+                          songName: songInfoListFuture[0]['title'],
+                          artistName: songInfoListFuture[0]['artist'],
+                          albumName: songInfoListFuture[0]['album'],
+                          dj: "Example DJ",
+                          lastPlayed: songInfoListFuture[0]
+                              ['last_played_timestamp'],
+                          imageUrl: songInfoListFuture[0]['art_url'],
+                          artistSpotifyID: songInfoListFuture[0]
+                              ['artist_spotify_id'],
+                          songSpotifyID: songInfoListFuture[0]
+                              ['song_spotify_id'],
+                        );
+
+                        return ListView(
+                          children: [
+                            Padding(
+                                padding:
+                                    EdgeInsets.only(top: deviceHeight * 0.05),
+                                child: FadeInImage.assetNetwork(
+                                  image: currentSong.imageUrl,
+                                  placeholder: 'assets/images/loading.gif',
+                                  imageErrorBuilder:
+                                      (context, error, stackTrace) {
+                                    //if the image fails to load, it will display a placeholder image.
+                                    return Image.asset(
+                                        'assets/images/loading.gif');
+                                  },
+                                  width: 200,
+                                  height: 200,
+                                )),
+                            titleSection(currentSong),
+                            playButtonSection,
+                            buttonSection(currentSong),
+                            textSection(currentSong),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      }
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }))),
         debugShowCheckedModeBanner: false);
   }
 
@@ -142,7 +180,7 @@ class _RadioPageState extends State<RadioPage> {
   InkWell _buildPlayButton(Color color, IconData icon, String text) {
     double deviceWidth = MediaQuery.of(context).size.width;
     double deviceHeight = MediaQuery.of(context).size.height;
-    /*
+
     FlutterRadioPlayer newRadio = FlutterRadioPlayer();
     newRadio.init("Radio", "Live",
         "https://wyms.streamguys1.com/live?platform=88nine", "false");
@@ -151,7 +189,7 @@ class _RadioPageState extends State<RadioPage> {
     } else {
       newRadio.pause();
     }
-    */
+
     return InkWell(
         onTap: () {
           if (playing) {
@@ -168,7 +206,6 @@ class _RadioPageState extends State<RadioPage> {
             height: deviceHeight * 0.2,
             decoration:
                 BoxDecoration(color: Colors.amber, shape: BoxShape.circle),
-            padding: EdgeInsets.only(left: 8, right: 8),
             child: Row(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
---
+++ multimedia/lib/songs.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Songs {
+  List<dynamic> songs;
+
+  Songs({required this.songs});
+
+  factory Songs.fromJson(Map<String, dynamic> json) {
+    return Songs(songs: json['songs']);
+  }
+}
+
+class SongInfo {
+  String songName;
+  String artistName;
+  String albumName;
+  String dj = "Example DJ";
+  int lastPlayed;
+  String imageUrl;
+  String artistSpotifyID;
+  String? songSpotifyID;
+
+  SongInfo(
+      {required this.songName,
+      required this.artistName,
+      required this.albumName,
+      required this.dj,
+      required this.lastPlayed,
+      required this.imageUrl,
+      required this.artistSpotifyID,
+      required this.songSpotifyID});
+}
+
+Future<Songs> fetchSongs() async {
+  final response = await http.get(Uri.parse(
+      'https://radio-mke-playlist-updater.herokuapp.com/playlist-history'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Songs.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load songs');
+  }
+}
